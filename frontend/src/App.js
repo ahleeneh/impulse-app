5e06c1cba572003f60ae149db0eb7c5e9605663a
Aline Murillo <alinemurillo33@gmail.com>
2023-08-28 19:02:11 -0500

    Add structuring to User Dashboard for visibility

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,23 +5,37 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
-import Navigation from './components/Navigation';
+import BudgetPage from './pages/BudgetPage';
+
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
 
   return (
     <div className="App">
+      <ToastContainer
+                position="top-center"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
 
       <BrowserRouter>
         <div className="App-container">
 
           <main>
-            {/* <Navigation/> */}
-
             <Routes>
               <Route path='/' element={<HomePage/>}></Route>
               <Route path='/register' element={<RegisterPage/>}></Route>
               <Route path='/dashboard' element={<DashboardPage/>}></Route>
+              <Route path='/budget' element={<BudgetPage/>}></Route>
             </Routes>
           </main>
 
