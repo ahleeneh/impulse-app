3ec61f8b81bf1e91d13d412f51f0b5356f9f7388
Aline Murillo <alinemurillo33@gmail.com>
2023-08-29 18:14:44 -0500

    Add custom hook to useUserBudget to retrieve user budget from backend

--- frontend/src/pages/BudgetPage.jsx
+++ frontend/src/pages/BudgetPage.jsx
@@ -1,34 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 import BudgetIncome from '../components/BudgetIncome';
 import BudgetHousing from '../components/BudgetHousing';
 import BudgetRecurring from '../components/BudgetRecurring';
 import BudgetAdditional from '../components/BudgetAdditional';
 import UserPageWrapper from '../components/UserPageWrapper';
+import useUserBudget from '../hooks/useUserBudget';
 
 function BudgetPage() {
-    const [userBudget, setUserBudget] = useState([])
-
-    const getUserBudget = async () => {
-        try {
-            const response = await axios.get('/budget');
-            if (response.status === 200) {
-                setUserBudget(response.data);
-                console.log('userBudget: ', response.data);
-            }
-        } catch (error) {
-            console.log('uh oh!!');
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        getUserBudget();
-    }, []);
+    const {userBudget, refreshBudget } = useUserBudget();
 
     return (
         <UserPageWrapper pageName="User Budget">
-            <BudgetIncome userBudget={userBudget} refreshBudget={getUserBudget} />
+            <BudgetIncome userBudget={userBudget} refreshBudget={refreshBudget} />
             <BudgetHousing userBudget={userBudget} />
             <BudgetRecurring userBudget={userBudget} />
             <BudgetAdditional userBudget={userBudget} />
--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import UserOverview from '../components/UserOverview';
 import UserGoals from '../components/UserGoals';
 import UserPageWrapper from '../components/UserPageWrapper';
+import useUserBudget from '../hooks/useUserBudget';
 
 function DashboardPage() {
+    const {userBudget, refreshBudget} = useUserBudget();
+
     return (
         <UserPageWrapper pageName="User Dashboard">
-            <UserOverview />
-            <UserGoals />
+            <UserOverview userBudget={userBudget} refreshBudget={refreshBudget} />
+            <UserGoals userBudget={userBudget} refreshBudget={refreshBudget} />
         </UserPageWrapper>
     );
 
