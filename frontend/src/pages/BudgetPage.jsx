d85f5a195df46c5d1b0c6ac28d1a6ab9605bdf2c
Aline Murillo <alinemurillo33@gmail.com>
2023-08-29 21:05:47 -0500

    Removed filteredItems in BudgetComponent as this is no longer needed as the Budget schema only has 1 month/year assigned to it per object

--- frontend/src/pages/BudgetPage.jsx
+++ frontend/src/pages/BudgetPage.jsx
@@ -1,20 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserPageWrapper from '../components/UserPageWrapper';
 import BudgetComponent from '../components/BudgetComponent';
 import useUserBudget from '../hooks/useUserBudget';
 
-
 function BudgetPage() {
-    const currentMonth = new Date().getMonth() + 1;
-    const currentYear = new Date().getFullYear();
-    const { userBudget, refreshBudget } = useUserBudget(currentMonth, currentYear);
+    const [selectedMonth, setSelectedMonth] = useState(new Date().getMonth() + 1);
+    const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+    const { userBudget, refreshBudget } = useUserBudget(selectedMonth, selectedYear);
+
+    const handleMonthChange = (event) => {
+        setSelectedMonth(parseInt(event.target.value));
+    }
+
+    const handleYearChange = (event) => {
+        setSelectedYear(parseInt(event.target.value));
+    }
 
     return (
         <UserPageWrapper pageName="User Budget">
-            <BudgetComponent userBudget={userBudget} refreshBudget={refreshBudget} category="income" />
-            <BudgetComponent userBudget={userBudget} refreshBudget={refreshBudget} category="housing" />
-            <BudgetComponent userBudget={userBudget} refreshBudget={refreshBudget} category="recurring" />
-            <BudgetComponent userBudget={userBudget} refreshBudget={refreshBudget} category="additional" />
+
+            <div className="select-date">
+                <label>Select Month:</label>
+                <input type="number" value={selectedMonth} onChange={handleMonthChange} />
+
+                <label>Select Year:</label>
+                <input type="number" value={selectedYear} onChange={handleYearChange} />
+            </div>
+
+            <BudgetComponent
+                category="income"
+                userBudget={userBudget}
+                refreshBudget={refreshBudget}
+                selectedMonth={selectedMonth}
+                selectedYear={selectedYear} />
+            <BudgetComponent
+                category="housing"
+                userBudget={userBudget}
+                refreshBudget={refreshBudget} 
+                selectedMonth={selectedMonth}
+                selectedYear={selectedYear} />
+            <BudgetComponent
+                category="recurring"
+                userBudget={userBudget}
+                refreshBudget={refreshBudget} 
+                selectedMonth={selectedMonth}
+                selectedYear={selectedYear} />
+            <BudgetComponent
+                category="additional"
+                userBudget={userBudget}
+                refreshBudget={refreshBudget} 
+                selectedMonth={selectedMonth}
+                selectedYear={selectedYear} />
+
         </UserPageWrapper>
     );
 
